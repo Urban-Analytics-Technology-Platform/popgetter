388dbdab308dee6caff58336bdc822d615235665
Stuart Lynn <stuart.lynn@gmail.com>
2024-04-12 18:42:29 +0100

    Adding types from pydantic json schema

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+use serde::{Deserialize, Serialize};
+use typify::import_types;
+import_types!("schema.json");
+
 pub fn hi() -> String {
     "Hello!".into()
+}
+
+pub fn random_survey() -> CountryMetadata {
+    CountryMetadata {
+        iso2: "US".into(),
+        iso3: "USA".into(),
+        name_official: "United States of America".into(),
+        name_short_en: "USA".into(),
+    }
 }
