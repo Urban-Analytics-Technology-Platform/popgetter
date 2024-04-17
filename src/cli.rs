5c03425f37b82882e13804fd5e0dc3d11e0937a2
Stuart Lynn <stuart.lynn@gmail.com>
2024-04-17 12:18:26 +0100

    Adding output format with automatic parsing

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,9 @@
 use std::str::FromStr;
 
 use clap::{Args, Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+use serde_json;
+use strum_macros::EnumString;
 
 #[derive(Clone, Debug)]
 pub struct BBox(pub [f64; 4]);
@@ -22,6 +25,15 @@ impl FromStr for BBox {
     }
 }
 
+#[derive(Clone,Debug,Deserialize,Serialize,EnumString,PartialEq, Eq)]
+#[strum(ascii_case_insensitive)]
+pub enum OutputFormat{
+    GeoJSON,
+    CSV,
+    GeoParquet,
+    FlatGeobuf,
+}
+
 #[derive(Args, Debug)]
 pub struct DataArgs {
     /// Only get data in  bounding box ([min_lat,min_lng,max_lat,max_lng])
@@ -40,6 +52,9 @@ pub struct MetricArgs {
     /// Only get the specific metrics
     #[arg(short, long)]
     filter: Option<String>,
+    /// Specify output format
+    #[arg(short, long)]
+    output_format: OutputFormat,
 }
 
 #[derive(Parser, Debug)]
@@ -63,4 +78,38 @@ pub enum Commands {
     Metrics(MetricArgs),
     /// Surveys
     Surveys,
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bbox_should_parse_if_correct() {
+        let bbox = BBox::from_str("0.0,1.0,2.0,3.0");
+        assert!(bbox.is_ok(), "A four coord bbox should parse");
+    }
+
+    #[test]
+    fn bbox_should_not_parse_if_incorrect() {
+        let bbox = BBox::from_str("0.0,1.0,2.0");
+        assert!(bbox.is_err(), "A string with fewer than 4 coords should parse");
+        let bbox = BBox::from_str("0.0,1.0,2.0,3.0,4.0");
+        assert!(bbox.is_err(), "A string with fewer than 5 coords should parse");
+        let bbox = BBox::from_str("0.0sdfsd,1.0,2.0");
+        assert!(bbox.is_err(), "A string with letters shouldn't parse");
+    }
+
+    #[test]
+    fn output_type_should_deserialize_properly(){
+        let output_format = OutputFormat::from_str("GeoJSON");
+        assert_eq!(output_format.unwrap(), OutputFormat::GeoJSON, "geojson format should be parsed correctly");
+        let output_format = OutputFormat::from_str("GeoJson");
+        assert_eq!(output_format.unwrap(), OutputFormat::GeoJSON, "parsing should be case insensitive");
+        let output_format = OutputFormat::from_str("geoparquet");
+        assert_eq!(output_format.unwrap(), OutputFormat::GeoParquet, "correct variants should parse correctly");
+        let output_format = OutputFormat::from_str("awesome_tiny_model");
+        assert!(output_format.is_err(), "non listed formats should fail");
+    }
 }
