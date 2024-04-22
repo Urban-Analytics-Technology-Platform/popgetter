7fc6b31c52a0b612aec0bc551c7e6c9a35fe81b4
Stuart Lynn <stuart.lynn@gmail.com>
2024-04-22 22:50:35 +0100

    Cargo fmt

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,17 @@
-// FromStr is required by EnumString. The compiler seems to not be able to 
+// FromStr is required by EnumString. The compiler seems to not be able to
 // see that and so is giving a warning. Dont remove it
-use std::str::FromStr;
-use clap::{Args, Parser, Subcommand};
-use serde::{Deserialize, Serialize};
-use strum_macros::EnumString;
-use popgetter::data_request_spec::{BBox, DataRequestSpec,  RegionSpec};
 use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
 use enum_dispatch::enum_dispatch;
+use popgetter::data_request_spec::{BBox, DataRequestSpec, RegionSpec};
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+use strum_macros::EnumString;
 
 /// Defines the output formats we are able to produce data in.
-#[derive(Clone,Debug,Deserialize,Serialize,EnumString,PartialEq, Eq)]
+#[derive(Clone, Debug, Deserialize, Serialize, EnumString, PartialEq, Eq)]
 #[strum(ascii_case_insensitive)]
-pub enum OutputFormat{
+pub enum OutputFormat {
     GeoJSON,
     Csv,
     GeoParquet,
@@ -20,19 +20,19 @@ pub enum OutputFormat{
 
 /// Trait that defines what to run when a given subcommand is invoked.
 #[enum_dispatch]
-pub trait RunCommand{
-    fn run(&self)->Result<()>;
-} 
+pub trait RunCommand {
+    fn run(&self) -> Result<()>;
+}
 
-/// The Data command is the one we invoke to get a set of metrics and geometry 
-/// for some given region and set of metrics. Currently it takes two arguments 
-/// - bbox: A Bounding box 
+/// The Data command is the one we invoke to get a set of metrics and geometry
+/// for some given region and set of metrics. Currently it takes two arguments
+/// - bbox: A Bounding box
 /// - metrics: A comma seperated list of metrics to retrive.
 ///
 /// The Data command can be converted into a `DataRequestSpec` which is the processed
 /// by the core library.
 #[derive(Args, Debug)]
-pub struct DataCommand{
+pub struct DataCommand {
     /// Only get data in  bounding box ([min_lat,min_lng,max_lat,max_lng])
     #[arg(short, long)]
     bbox: Option<BBox>,
@@ -44,35 +44,30 @@ pub struct DataCommand{
     output_format: OutputFormat,
 }
 
-impl RunCommand for DataCommand{
-    fn run(&self)->Result<()> {
+impl RunCommand for DataCommand {
+    fn run(&self) -> Result<()> {
         println!("Running Data Command");
         println!("{self:#?}");
         Ok(())
     }
 }
 
-impl From<DataCommand> for DataRequestSpec{
-        fn from(value: DataCommand) -> Self {
-            let region = if let Some(bbox) = value.bbox{
-                vec![RegionSpec::BoundingBox(bbox)] 
-            }
-            else{
-                vec![]
-            };
-            let metrics = vec![];
-            DataRequestSpec{
-                region,
-                metrics
-            }
-
+impl From<DataCommand> for DataRequestSpec {
+    fn from(value: DataCommand) -> Self {
+        let region = if let Some(bbox) = value.bbox {
+            vec![RegionSpec::BoundingBox(bbox)]
+        } else {
+            vec![]
+        };
+        let metrics = vec![];
+        DataRequestSpec { region, metrics }
     }
 }
 
 /// The Metrics command allows a user to search for a set of metrics by bounding box and filter.
 /// The set of ways to search will likley increase over time
 #[derive(Args, Debug)]
-pub struct MetricsCommand{
+pub struct MetricsCommand {
     /// Only get data in  bounding box ([min_lat,min_lng,max_lat,max_lng])
     #[arg(short, long)]
     bbox: Option<BBox>,
@@ -81,8 +76,8 @@ pub struct MetricsCommand{
     filter: Option<String>,
 }
 
-impl RunCommand for MetricsCommand{
-    fn run(&self)->Result<()> {
+impl RunCommand for MetricsCommand {
+    fn run(&self) -> Result<()> {
         println!("Running Metrics Command");
         Ok(())
     }
@@ -90,11 +85,11 @@ impl RunCommand for MetricsCommand{
 
 /// The Countries command should return information about the various countries we have data for.
 /// This could include metrics like the number of surveys / metrics / geographies avaliable for each country.
-#[derive(Args,Debug)]
+#[derive(Args, Debug)]
 pub struct CountriesCommand;
 
-impl RunCommand for CountriesCommand{
-    fn run(&self)->Result<()> {
+impl RunCommand for CountriesCommand {
+    fn run(&self) -> Result<()> {
         println!("Running Countries Command");
         Ok(())
     }
@@ -102,11 +97,11 @@ impl RunCommand for CountriesCommand{
 
 /// The Surveys command should list the various surveys that popgetter has access to and releated
 /// stastistics.
-#[derive(Args,Debug)]
+#[derive(Args, Debug)]
 pub struct SurveysCommand;
 
-impl RunCommand for SurveysCommand{
-    fn run(&self)->Result<()> {
+impl RunCommand for SurveysCommand {
+    fn run(&self) -> Result<()> {
         println!("Running Surveys Command");
         Ok(())
     }
@@ -114,20 +109,14 @@ impl RunCommand for SurveysCommand{
 
 /// The entrypoint for the CLI.
 #[derive(Parser, Debug)]
-#[command(version, 
-          about, 
-          long_about = None, 
-          name="popgetter", 
-          long_about="Popgetter is a tool to quickly get the data you need!"
-)]
+#[command(version, about, long_about = None, name="popgetter", long_about="Popgetter is a tool to quickly get the data you need!")]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Option<Commands>,
 }
 
-
-/// Commands contains the list of subcommands avaliable for use in the CLI. 
-/// Each command should implmement the RunCommand trait and specify the list 
+/// Commands contains the list of subcommands avaliable for use in the CLI.
+/// Each command should implmement the RunCommand trait and specify the list
 /// of required args for that command.
 #[derive(Subcommand, Debug)]
 #[enum_dispatch(RunCommand)]
@@ -146,13 +135,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn output_type_should_deserialize_properly(){
+    fn output_type_should_deserialize_properly() {
         let output_format = OutputFormat::from_str("GeoJSON");
-        assert_eq!(output_format.unwrap(), OutputFormat::GeoJSON, "geojson format should be parsed correctly");
+        assert_eq!(
+            output_format.unwrap(),
+            OutputFormat::GeoJSON,
+            "geojson format should be parsed correctly"
+        );
         let output_format = OutputFormat::from_str("GeoJson");
-        assert_eq!(output_format.unwrap(), OutputFormat::GeoJSON, "parsing should be case insensitive");
+        assert_eq!(
+            output_format.unwrap(),
+            OutputFormat::GeoJSON,
+            "parsing should be case insensitive"
+        );
         let output_format = OutputFormat::from_str("geoparquet");
-        assert_eq!(output_format.unwrap(), OutputFormat::GeoParquet, "correct variants should parse correctly");
+        assert_eq!(
+            output_format.unwrap(),
+            OutputFormat::GeoParquet,
+            "correct variants should parse correctly"
+        );
         let output_format = OutputFormat::from_str("awesome_tiny_model");
         assert!(output_format.is_err(), "non listed formats should fail");
     }
