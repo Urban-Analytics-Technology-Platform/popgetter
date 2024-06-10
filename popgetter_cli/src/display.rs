cd221b64723d90dab067de032aa840f6a118bc84
Stuart Lynn <stuart.lynn@gmail.com>
2024-06-10 10:58:55 +0100

    Moving to worspace structure

---
+++ popgetter_cli/src/display.rs
@@ -0,0 +1,52 @@
+use comfy_table::{presets::NOTHING, *};
+use itertools::izip;
+use popgetter::search::SearchResults;
+
+pub fn display_search_results(results: SearchResults, max_results: Option<usize>) {
+    let df_to_show = match max_results {
+        Some(max) => results.0.head(Some(max)),
+        None => results.0,
+    };
+
+    for (metric_id, hrn, desc, hxl, level) in izip!(
+        df_to_show.column("metric_id").unwrap().iter(),
+        df_to_show.column("human_readable_name").unwrap().iter(),
+        df_to_show.column("metric_description").unwrap().iter(),
+        df_to_show.column("metric_hxl_tag").unwrap().iter(),
+        df_to_show.column("geometry_level").unwrap().iter(),
+    ) {
+        let mut table = Table::new();
+        table
+            .load_preset(NOTHING)
+            .set_content_arrangement(ContentArrangement::Dynamic)
+            .set_style(comfy_table::TableComponent::BottomBorder, '─')
+            .set_style(comfy_table::TableComponent::BottomBorderIntersections, '─')
+            .set_style(comfy_table::TableComponent::TopBorder, '─')
+            .set_style(comfy_table::TableComponent::TopBorderIntersections, '─')
+            .add_row(vec![
+                Cell::new("Metric ID").add_attribute(Attribute::Bold),
+                metric_id.get_str().unwrap().into(),
+            ])
+            .add_row(vec![
+                Cell::new("Human readable name").add_attribute(Attribute::Bold),
+                hrn.get_str().unwrap().into(),
+            ])
+            .add_row(vec![
+                Cell::new("Description").add_attribute(Attribute::Bold),
+                desc.get_str().unwrap().into(),
+            ])
+            .add_row(vec![
+                Cell::new("HXL tag").add_attribute(Attribute::Bold),
+                hxl.get_str().unwrap().into(),
+            ])
+            .add_row(vec![
+                Cell::new("Geometry level").add_attribute(Attribute::Bold),
+                level.get_str().unwrap().into(),
+            ]);
+
+        let column = table.column_mut(0).unwrap();
+        column.set_cell_alignment(CellAlignment::Right);
+
+        println!("\n{}", table);
+    }
+}
---
+++ popgetter_cli/src/main.rs
@@ -0,0 +1,40 @@
+mod cli;
+mod display;
+
+use anyhow::Result;
+use clap::Parser;
+use cli::{Cli, RunCommand};
+use log::debug;
+use popgetter::config::Config;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    pretty_env_logger::init_timed();
+
+    let args = Cli::parse();
+    debug!("args: {args:?}");
+    let config: Config = read_config_from_toml();
+    debug!("config: {config:?}");
+
+    if let Some(command) = args.command {
+        command.run(config).await?;
+    }
+    Ok(())
+}
+
+fn read_config_from_toml() -> Config {
+    // macOS: ~/.config/popgetter/config.toml
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("popgetter").unwrap();
+    let file_path = xdg_dirs.place_config_file("config.toml").unwrap();
+
+    match std::fs::read_to_string(file_path) {
+        Ok(contents) => toml::from_str(&contents).expect("Invalid TOML in config file"),
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::NotFound {
+                Config::default()
+            } else {
+                panic!("Error reading config file: {:#?}", e);
+            }
+        }
+    }
+}
