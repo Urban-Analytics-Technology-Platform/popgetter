aaae45b6228702627dfef98490be41e8ce25cc9d
Stuart Lynn <stuart.lynn@gmail.com>
2024-06-12 13:43:26 +0100

    Format

--- popgetter_py/src/lib.rs
+++ popgetter_py/src/lib.rs
@@ -1,19 +1,31 @@
 use std::default::Default;
 
 use ::popgetter::{
-    data_request_spec::{DataRequestSpec, GeometrySpec, MetricSpec}, 
-    search::{SearchContext, SearchRequest, SearchResults, SearchText}};
-use pyo3::{prelude::*, types::{PyDict, PyString}};
-use pyo3_polars::PyDataFrame;
+    data_request_spec::{DataRequestSpec, GeometrySpec, MetricSpec},
+    search::{SearchContext, SearchRequest, SearchResults, SearchText},
+};
 use polars::prelude::DataFrame;
+use pyo3::{
+    prelude::*,
+    types::{PyDict, PyString},
+};
+use pyo3_polars::PyDataFrame;
 
-async fn _search()-> DataFrame{
+async fn _search() -> DataFrame {
     let search_request: SearchRequest = SearchRequest::default();
     let popgetter = ::popgetter::Popgetter::new().await.unwrap();
     let search_results = popgetter.search(&search_request).await.unwrap();
-    search_results.0.select(&["metric_id","human_readable_name", "metric_description", "metric_hxl_tag","geometry_level"]).unwrap()
+    search_results
+        .0
+        .select(&[
+            "metric_id",
+            "human_readable_name",
+            "metric_description",
+            "metric_hxl_tag",
+            "geometry_level",
+        ])
+        .unwrap()
 }
-
 
 // async fn _data()-> DataFrame{
 //     let search_request: SearchRequest = SearchRequest::default();
@@ -22,63 +34,69 @@ async fn _search()-> DataFrame{
 // }
 
 fn get_search(obj: &Bound<'_, PyAny>) -> PyResult<SearchRequest> {
-
-    if let Ok(text) = obj.downcast::<PyString>(){
-        println!("object is a string {}",text);
-        let search_text = SearchText{
+    if let Ok(text) = obj.downcast::<PyString>() {
+        println!("object is a string {}", text);
+        let search_text = SearchText {
             text: text.to_string(),
             ..SearchText::default()
         };
-        return Ok(SearchRequest{
-            text:vec![search_text],
+        return Ok(SearchRequest {
+            text: vec![search_text],
             ..SearchRequest::default()
-        })
+        });
     }
 
-    if let Ok(dict)= obj.downcast::<PyDict>(){
-        println!("Object is a dict {}",dict);
-        return Ok(SearchRequest::default())
+    if let Ok(dict) = obj.downcast::<PyDict>() {
+        println!("Object is a dict {}", dict);
+        return Ok(SearchRequest::default());
     };
 
     Ok(SearchRequest::default())
 }
 
 fn get_data_request(obj: &Bound<'_, PyAny>) -> PyResult<DataRequestSpec> {
-    Ok(DataRequestSpec{
-        geometry: GeometrySpec{include_geoms:true, geometry_level:None},
+    Ok(DataRequestSpec {
+        geometry: GeometrySpec {
+            include_geoms: true,
+            geometry_level: None,
+        },
         region: vec![],
-        metrics: vec![MetricSpec::Metric(::popgetter::metadata::MetricId::Hxl(r"\#population\+adults".into()))],
+        metrics: vec![MetricSpec::Metric(::popgetter::metadata::MetricId::Hxl(
+            r"\#population\+adults".into(),
+        ))],
         years: None,
     })
 }
 
-async fn _get_data(data_request: &DataRequestSpec)->DataFrame{
-    
+async fn _get_data(data_request: &DataRequestSpec) -> DataFrame {
     let popgetter = ::popgetter::Popgetter::new().await.unwrap();
-    println!("running data request {:#?}",data_request);
+    println!("running data request {:#?}", data_request);
     popgetter.get_data_request(data_request).await.unwrap()
 }
 
 #[pyfunction]
-fn get(#[pyo3(from_py_with = "get_data_request")] data_request: DataRequestSpec)->PyResult<PyDataFrame>{
-    
-   let rt = tokio::runtime::Builder::new_current_thread()
+fn get(
+    #[pyo3(from_py_with = "get_data_request")] data_request: DataRequestSpec,
+) -> PyResult<PyDataFrame> {
+    let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?;
 
-        // Call the asynchronous connect method using the runtime.
-    let result= rt.block_on(_get_data(&data_request));
+    // Call the asynchronous connect method using the runtime.
+    let result = rt.block_on(_get_data(&data_request));
     Ok(PyDataFrame(result))
 }
 
 #[pyfunction]
-fn search(#[pyo3(from_py_with = "get_search")] search_query: SearchRequest) -> PyResult<PyDataFrame> {
-   let rt = tokio::runtime::Builder::new_current_thread()
+fn search(
+    #[pyo3(from_py_with = "get_search")] search_query: SearchRequest,
+) -> PyResult<PyDataFrame> {
+    let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?;
 
-        // Call the asynchronous connect method using the runtime.
-    let result= rt.block_on(_search());
+    // Call the asynchronous connect method using the runtime.
+    let result = rt.block_on(_search());
     Ok(PyDataFrame(result))
 }
 
