522c37073b71afc1f17bbce9fbb0a7424d735a88
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-07-04 17:15:56 +0100

    Refactor struct as module with re-export

--- src/lib.rs
+++ src/lib.rs
@@ -7,85 +7,21 @@ use polars::{frame::DataFrame, prelude::DataFrameJoinOps};
 use tokio::try_join;
 
 use crate::{config::Config, geo::get_geometries};
+
+// Re-exports
+pub use column_names as COL;
+
+// Modules
+pub mod column_names;
 pub mod config;
 pub mod data_request_spec;
 pub mod error;
+#[cfg(feature = "formatters")]
+pub mod formatters;
 pub mod geo;
 pub mod metadata;
 pub mod parquet;
 pub mod search;
-
-/// This struct stores the eventual column names of all the metadata classes, which are used when
-/// serialising the metadata to a dataframe. Note that this must be synchronised with column names
-/// defined in the upstream metadata classes!
-pub struct COL;
-
-impl COL {
-    pub const GEO_ID: &'static str = "GEO_ID";
-
-    pub const COUNTRY_ID: &'static str = "country_id";
-    pub const COUNTRY_NAME_SHORT_EN: &'static str = "country_name_short_en";
-    pub const COUNTRY_NAME_OFFICIAL: &'static str = "country_name_official";
-    pub const COUNTRY_ISO3: &'static str = "country_iso3";
-    pub const COUNTRY_ISO2: &'static str = "country_iso2";
-    pub const COUNTRY_ISO3166_2: &'static str = "country_iso3166_2";
-
-    pub const DATA_PUBLISHER_ID: &'static str = "data_publisher_id";
-    pub const DATA_PUBLISHER_NAME: &'static str = "data_publisher_name";
-    pub const DATA_PUBLISHER_URL: &'static str = "data_publisher_url";
-    pub const DATA_PUBLISHER_DESCRIPTION: &'static str = "data_publisher_description";
-    pub const DATA_PUBLISHER_COUNTRIES_OF_INTEREST: &'static str =
-        "data_publisher_countries_of_interest";
-
-    pub const GEOMETRY_ID: &'static str = "geometry_id";
-    pub const GEOMETRY_FILEPATH_STEM: &'static str = "geometry_filepath_stem";
-    pub const GEOMETRY_VALIDITY_PERIOD_START: &'static str = "geometry_validity_period_start";
-    pub const GEOMETRY_VALIDITY_PERIOD_END: &'static str = "geometry_validity_period_end";
-    pub const GEOMETRY_LEVEL: &'static str = "geometry_level";
-    pub const GEOMETRY_HXL_TAG: &'static str = "geometry_hxl_tag";
-
-    pub const SOURCE_DATA_RELEASE_ID: &'static str = "source_data_release_id";
-    pub const SOURCE_DATA_RELEASE_NAME: &'static str = "source_data_release_name";
-    pub const SOURCE_DATA_RELEASE_DATE_PUBLISHED: &'static str =
-        "source_data_release_date_published";
-    pub const SOURCE_DATA_RELEASE_REFERENCE_PERIOD_START: &'static str =
-        "source_data_release_reference_period_start";
-    pub const SOURCE_DATA_RELEASE_REFERENCE_PERIOD_END: &'static str =
-        "source_data_release_reference_period_end";
-    pub const SOURCE_DATA_RELEASE_COLLECTION_PERIOD_START: &'static str =
-        "source_data_release_collection_period_start";
-    pub const SOURCE_DATA_RELEASE_COLLECTION_PERIOD_END: &'static str =
-        "source_data_release_collection_period_end";
-    pub const SOURCE_DATA_RELEASE_EXPECT_NEXT_UPDATE: &'static str =
-        "source_data_release_expect_next_update";
-    pub const SOURCE_DATA_RELEASE_URL: &'static str = "source_data_release_url";
-    pub const SOURCE_DATA_RELEASE_DATA_PUBLISHER_ID: &'static str =
-        "source_data_release_data_publisher_id";
-    pub const SOURCE_DATA_RELEASE_DESCRIPTION: &'static str = "source_data_release_description";
-    pub const SOURCE_DATA_RELEASE_GEOMETRY_METADATA_ID: &'static str =
-        "source_data_release_geometry_metadata_id";
-
-    pub const METRIC_ID: &'static str = "metric_id";
-    pub const METRIC_HUMAN_READABLE_NAME: &'static str = "metric_human_readable_name";
-    pub const METRIC_SOURCE_METRIC_ID: &'static str = "metric_source_id";
-    pub const METRIC_DESCRIPTION: &'static str = "metric_description";
-    pub const METRIC_HXL_TAG: &'static str = "metric_hxl_tag";
-    pub const METRIC_PARQUET_PATH: &'static str = "metric_parquet_path";
-    pub const METRIC_PARQUET_COLUMN_NAME: &'static str = "metric_parquet_column_name";
-    pub const METRIC_PARQUET_MARGIN_OF_ERROR_COLUMN: &'static str =
-        "metric_parquet_margin_of_error_column";
-    pub const METRIC_PARQUET_MARGIN_OF_ERROR_FILE: &'static str =
-        "metric_parquet_margin_of_error_file";
-    pub const METRIC_POTENTIAL_DENOMINATOR_IDS: &'static str = "metric_potential_denominator_ids";
-    pub const METRIC_PARENT_METRIC_ID: &'static str = "metric_parent_metric_id";
-    pub const METRIC_SOURCE_DATA_RELEASE_ID: &'static str = "metric_source_data_release_id";
-    pub const METRIC_SOURCE_DOWNLOAD_URL: &'static str = "metric_source_download_url";
-    pub const METRIC_SOURCE_ARCHIVE_FILE_PATH: &'static str = "metric_source_archive_file_path";
-    pub const METRIC_SOURCE_DOCUMENTATION_URL: &'static str = "metric_source_documentation_url";
-}
-
-#[cfg(feature = "formatters")]
-pub mod formatters;
 
 pub struct Popgetter {
     pub metadata: Metadata,
