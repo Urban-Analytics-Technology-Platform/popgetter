9201f46152179e10cb6206165951a535aeb47293
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-07-12 07:48:43 +0100

    Revise version naming in config base path

--- src/config.rs
+++ src/config.rs
@@ -9,7 +9,9 @@ pub struct Config {
 impl Default for Config {
     fn default() -> Self {
         Config {
-            base_path: "https://popgetter.blob.core.windows.net/releases/v0.2.0".into(),
+            // TODO: add fn to generate the release directory name from the CLI version directly
+            // E.g. this could be achieved with: https://docs.rs/built/latest/built/
+            base_path: "https://popgetter.blob.core.windows.net/releases/v0.2".into(),
         }
     }
 }
