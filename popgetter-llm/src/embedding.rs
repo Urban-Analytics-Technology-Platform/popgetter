7418b3e88aaf76ef559644b8e823e498ce497dab
Sam Greenbury <sgreenbury@turing.ac.uk>
2024-10-29 21:39:52 +0000

    Refactor main with clap

--- popgetter-llm/src/embedding.rs
+++ popgetter-llm/src/embedding.rs
@@ -9,29 +9,10 @@ use langchain_rust::{
     vectorstore::{qdrant::Store, VecStoreOptions, VectorStore},
 };
 use popgetter::{Popgetter, COL};
+use rand::{rngs::StdRng, seq::IteratorRandom, SeedableRng};
 use serde_json::Value;
 
-pub async fn init_embeddings(
-    // client: &Qdrant,
-    // embedder: OpenAiEmbedder<AzureConfig>,
-    store: &mut Store,
-) -> anyhow::Result<()> {
-    // Initialize Embedder
-    // let embedder = azure_open_ai_embedding(&api_key()?);
-
-    // // Initialize the qdrant_client::Qdrant
-    // // Ensure Qdrant is running at localhost, with gRPC port at 6334
-    // // docker run -p 6334:6334 qdrant/qdrant
-    // let client = Qdrant::from_url("http://localhost:6334").build().unwrap();
-
-    // let store = StoreBuilder::new()
-    //     .embedder(embedder)
-    //     .client(client)
-    //     .collection_name("popgetter_1000_400")
-    //     .build()
-    //     .await
-    //     .unwrap();
-
+pub async fn init_embeddings(store: &mut Store) -> anyhow::Result<()> {
     let popgetter = Popgetter::new_with_config_and_cache(Default::default()).await?;
     let combined_metadata = popgetter
         .metadata
@@ -44,12 +25,19 @@ pub async fn init_embeddings(
         combined_metadata
             .column(COL::METRIC_HUMAN_READABLE_NAME)?
             .str()?
-            .into_iter(),
+            .into_iter()
+            // TODO: make sample configurable
+            .choose_multiple(&mut StdRng::seed_from_u64(0), 1000),
         combined_metadata
             .column(COL::COUNTRY_NAME_SHORT_EN)?
             .str()?
-            .into_iter(),
-        combined_metadata.column(COL::METRIC_ID)?.str()?.into_iter()
+            .into_iter()
+            .choose_multiple(&mut StdRng::seed_from_u64(0), 1000),
+        combined_metadata
+            .column(COL::METRIC_ID)?
+            .str()?
+            .into_iter()
+            .choose_multiple(&mut StdRng::seed_from_u64(0), 1000)
     ) {
         let s: String = description.ok_or(anyhow!("Not a str"))?.into();
 
@@ -70,6 +58,7 @@ pub async fn init_embeddings(
         v.push(doc);
     }
 
+    // TODO: add rate limiting
     // Add documents to store
     store
         .add_documents(&v, &VecStoreOptions::default())
@@ -91,16 +80,5 @@ pub async fn query_embeddings(
         .await
         // TODO: fix error type
         .unwrap();
-
-    // TODO: Add filtering by metadata values (e.g. country)
-    // https://qdrant.tech/documentation/concepts/hybrid-queries/?q=color#re-ranking-with-payload-values
-    if results.is_empty() {
-        println!("No results found.");
-        return Ok(results);
-    } else {
-        results.iter().for_each(|r| {
-            println!("Document: {:#?}", r);
-        });
-    }
     Ok(results)
 }
--- popgetter-llm/src/main.rs
+++ popgetter-llm/src/main.rs
@@ -1,134 +1,72 @@
-// To run this example execute: cargo run --example vector_store_qdrant --features qdrant
-
-use langchain_rust::{
-    embedding::openai::openai_embedder::OpenAiEmbedder,
-    llm::AzureConfig,
-    schemas::Document,
-    vectorstore::{
-        qdrant::{Qdrant, StoreBuilder},
-        VectorStore,
-    },
+use clap::{Args, Parser, Subcommand};
+use langchain_rust::vectorstore::qdrant::{Qdrant, StoreBuilder};
+use popgetter_llm::{
+    embedding::{init_embeddings, query_embeddings},
+    utils::{api_key, azure_open_ai_embedding},
 };
-use serde_json::Value;
 
-use std::{collections::HashMap, io::Write};
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-use anyhow::anyhow;
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    Query(QueryArgs),
+}
 
-use popgetter::{Popgetter, COL};
+#[derive(Args)]
+struct QueryArgs {
+    #[arg(index = 1)]
+    query: String,
+    #[arg(long)]
+    limit: usize,
+}
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let color_map = HashMap::from([
-        ("Scotland", "red"),
-        ("Belgium", "green"),
-        ("Northern Ireland", "blue"),
-        ("United States", "yellow"),
-        ("England and Wales", "orange"),
-    ]);
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
 
     // Initialize Embedder
-
-    use langchain_rust::vectorstore::VecStoreOptions;
-
-    let endpoint = "https://demolandllm.openai.azure.com/";
-    let azure_config = AzureConfig::default()
-        .with_api_key(std::env::var("AZURE_OPEN_AI_KEY")?)
-        .with_api_base(endpoint)
-        .with_api_version("2023-05-15")
-        .with_deployment_id("popgetter");
-
-    let embedder = OpenAiEmbedder::new(azure_config);
-
-    // Embedding
-    // let result = embedder.embed_query("Why is the sky blue?").await.unwrap();
-    // println!("{:?}", result);
-    // println!("{:?}", result.len());
-
-    // Requires OpenAI API key to be set in the environment variable OPENAI_API_KEY
-    // let embedder = OpenAiEmbedder::default();
+    let embedder = azure_open_ai_embedding(&api_key()?);
 
     // Initialize the qdrant_client::Qdrant
     // Ensure Qdrant is running at localhost, with gRPC port at 6334
     // docker run -p 6334:6334 qdrant/qdrant
     let client = Qdrant::from_url("http://localhost:6334").build().unwrap();
 
-    let store = StoreBuilder::new()
+    // Init store
+    let mut store = StoreBuilder::new()
         .embedder(embedder)
         .client(client)
-        .collection_name("popgetter_1000_400")
+        .collection_name("popgetter")
         .build()
-        .await
-        .unwrap();
+        .await?;
 
-    let popgetter = Popgetter::new_with_config_and_cache(Default::default()).await?;
-    let combined_metadata = popgetter
-        .metadata
-        .combined_metric_source_geometry()
-        .0
-        .collect()?;
-    let mut v = vec![];
-    for (description, country) in combined_metadata
-        .column(COL::METRIC_HUMAN_READABLE_NAME)?
-        .str()?
-        .into_iter()
-        .zip(
-            combined_metadata
-                .column(COL::COUNTRY_NAME_SHORT_EN)?
-                .str()?
-                .into_iter(),
-        )
-        .step_by(400)
-        .take(1000)
-    {
-        let s: String = description.ok_or(anyhow!("Not a str"))?.into();
+    match cli.command {
+        Commands::Init => {
+            // Init embeddings
+            init_embeddings(&mut store).await?;
+        }
+        Commands::Query(query_args) => {
+            // TODO: see if we can subset similarity search by metadata values
+            let results = query_embeddings(&query_args.query, query_args.limit, &store).await?;
 
-        // TODO: add method to return HashMap of a row with keys (columns) and values
-        // Could just use the IDs and lookup in polars too.
-        let mut hm: HashMap<String, Value> = HashMap::new();
-        hm.insert(
-            "country".to_owned(),
-            Value::String(country.unwrap().to_string()),
-        );
-        hm.insert(
-            "color".to_owned(),
-            Value::String(color_map.get(country.unwrap()).unwrap().to_string()),
-        );
-        // TODO: add other metadata
-        let doc = Document::new(s).with_metadata(hm);
-        v.push(doc);
-    }
-
-    // // Add documents to the database
-    // TODO: make this `popgetter llm init`
-    // store
-    //     .add_documents(&v, &VecStoreOptions::default())
-    //     .await
-    //     .unwrap();
-
-    // `popgetter llm query "QUERY"`
-    loop {
-        // Ask for user input
-        print!("Query> ");
-        std::io::stdout().flush().unwrap();
-        let mut query = "".to_string();
-        std::io::stdin().read_line(&mut query).unwrap();
-
-        // TODO: see if we can subset similarity search by metadata values
-        let results = store
-            .similarity_search(&query, 4, &VecStoreOptions::default())
-            .await
-            .unwrap();
-
-        // TODO: Add filtering by metadata values (e.g. country)
-        // https://qdrant.tech/documentation/concepts/hybrid-queries/?q=color#re-ranking-with-payload-values
-        if results.is_empty() {
-            println!("No results found.");
-            return Ok(());
-        } else {
-            results.iter().for_each(|r| {
-                println!("Document: {:#?}", r);
-            });
+            // TODO: Add filtering by metadata values (e.g. country)
+            // https://qdrant.tech/documentation/concepts/hybrid-queries/?q=color#re-ranking-with-payload-values
+            if results.is_empty() {
+                println!("No results found.");
+                return Ok(());
+            } else {
+                results.iter().for_each(|r| {
+                    println!("Document: {:#?}", r);
+                });
+            }
         }
     }
+    Ok(())
 }
